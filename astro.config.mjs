8fca2e20bfae122b71f7c0bb6b948dee2af3b919
nakuwalar <nakuwalar@gmail.com>
2025-06-19 14:12:09 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // astro.config.mjs
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap'; // Import the sitemap integration
-
+import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   // Output mode default adalah 'static' (SSG), ini yang kita inginkan
   output: 'server',
-  
+  adapter: cloudflare(),
   // WAJIB: Ganti dengan URL situs Anda yang sebenarnya
   site: 'https://bokeporgasme.pages.dev', // Replace with your actual website domain for SEO
   
