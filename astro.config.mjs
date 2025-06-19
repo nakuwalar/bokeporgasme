3a292200aeeee18c9c72334f6a39b769ebd3fdca
nakuwalar <nakuwalar@gmail.com>
2025-06-19 22:58:50 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
   integrations: [sitemap({
-    changefreq: 'monthly',
+    changefreq: 'daily',
     priority: 1,
     })],
 });
