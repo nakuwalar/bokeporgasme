1aded025f5994ee2ae5695ec190238fe6148c08a
nakuwalar <nakuwalar@gmail.com>
2025-06-20 00:29:00 +0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 
 export default defineConfig({
+  site: 'https://bokeporgasme.pages.dev',
   output: 'server',
   adapter: cloudflare(),
   integrations: [sitemap({
