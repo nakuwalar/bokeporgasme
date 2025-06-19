e38f196908ad4aa7a9f4d17497a94cf8fe26ae86
nakuwalar <nakuwalar@gmail.com>
2025-06-19 22:54:01 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,8 @@ export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
   site: 'https://bokeporgasme.pages.dev',
-  integrations: [sitemap()],
+  integrations: [sitemap({
+    changefreq: 'monthly',
+    priority: 1,
+    })],
 });
